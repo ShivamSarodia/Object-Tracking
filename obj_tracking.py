2568d8623d0351d870b03f9f81c9ff3a10c9a1b9
Shivam Sarodia <ssarodia@gmail.com>
2015-06-29 02:24:29 -0400

    Implemented tracker tick method

--- obj_tracking.py
+++ obj_tracking.py
@@ -89,11 +89,19 @@ class Tracker:
         self.p1 = p1
         self.p2 = p2
         
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        self.points = cv2.goodFeaturesToTrack(gray, mask = None, **self.feature_params)
+        self.old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        self.points = cv2.goodFeaturesToTrack(self.old_gray, mask = None, **self.feature_params)
         
     def tick(self, frame):
-        pass
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        all_points, st, err = cv2.calcOpticalFlowPyrLK(self.old_gray, frame_gray, self.points, None, **lk_params)
+
+        #Select good points
+        good_new = all_points[st == 1]
+        good_old = all_points[st == 0]
+
+        self.old_gray = frame_gray.copy()
+        self.points = good_new.reshape(-1, 1, 2)
 
     def get_points(self):
         return self.points
@@ -141,6 +149,8 @@ while running:
         #rectangle has been selected
         if status == select.JUST_SELECTED: #if new selection, create new Tracker
             tracker = Tracker(frame, select.get_p1(), select.get_p2())
+        else: #if old selection, tick the tracker
+            tracker.tick(frame)
             
         rect_p1 = tracker.get_p1()
         rect_p2 = tracker.get_p2()
